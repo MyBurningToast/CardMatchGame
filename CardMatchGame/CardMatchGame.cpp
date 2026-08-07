c4bb7bf4f7bd5815bbc77c97f0a939ac0054361f
Max Martin <treblaxam@gmail.com>
2026-08-08 10:52:03 +1200

    Blank CMake C++ Project

---
+++ CardMatchGame/CardMatchGame.cpp
@@ -0,0 +1,12 @@
+﻿// CardMatchGame.cpp : Defines the entry point for the application.
+//
+
+#include "CardMatchGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
